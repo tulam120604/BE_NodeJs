bdce846b103d6216b5cf01a70b230fcd663fcc1e
tulam120604 <saudoi1204@gmail.com>
2025-02-18 19:58:55 +0700

    fix cors v2

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5000', 'https://fe-store88.vercel.app'],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
 }));
 app.use(cookieParser())
 

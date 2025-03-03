5efc6e5b7cb124afafe314f1a6e67de4b52edcdb
tulam120604 <saudoi1204@gmail.com>
2025-03-04 01:33:35 +0700

    update revenue

--- src/Controllers/Analytics/Analytics_product.js
+++ src/Controllers/Analytics/Analytics_product.js
@@ -12,26 +12,36 @@ export async function caculate_revenue(req, res) {
       },
       // {
       //   $match: {
-      //     "$items_order.product_id.id_user_seller.role": { $gt: "admin_global" },
+      //     "$items_order._id": "67c4790b88828d590bade3f5",
       //   },
       // },
       {
         $group: {
-          _id: null,
-          totalAmount: { $sum: "$items_order.total_price_item" },
+          _id: "$items_order.product_id.seller.role", // nhóm lại theo role để lọc
+          totalAmount: { $sum: "$items_order.total_price_item" }, // tính tổng doanh thu
+          nameSeller: {$first: "$items_order.product_id.seller.user_name"}, // tên người bán
+          emailSeller: {$first: "$items_order.product_id.seller.email"}, // email người bán
+          avatarSeller: {$first: "$items_order.product_id.seller.avatar"}, // avatar người bán
+          countItems: {$sum : 1}
         },
       },
     ]);
-    console.log(result);
     if (!id_user) {
       return res.status(StatusCodes.BAD_REQUEST).json({
         message: "No user!",
       });
     }
-    let total_revenue = 0;
     if (role === "seller") {
+      const data = result.filter(role => role.seller === 'seller');
+      return res.status(StatusCodes.OK).json({
+        message: 'OK',
+        data
+      }) 
     }
-    return total_revenue;
+    return res.status(StatusCodes.OK).json({
+      message : 'OK',
+      data : result
+    })
   } catch (error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       message: error,
